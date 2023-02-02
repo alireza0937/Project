b9f16ec406b4b0febbe5cb93556bc5bf7e257ee7
Alireza <alireza.bayrami@gmail.com>
2023-02-02 10:31:10 +0330

    reset

--- README.md
+++
@@ -1,4 +0,0 @@
-# password-generator
-This is a password-generator project, used in Quera's Git course.
-All Rights Reserved @ Quera
- 
--- generator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def generate_password(length, data):
-    password = ''
-    for i in range(length):
-        index = random.randint(0, len(data) - 1)
-        character = data[index]
-        password += character
-    return password
